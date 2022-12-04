338acc0bbaf26018df1d976cae0c67ba2de3b59e
oka0509 <okaoka1791@gmail.com>
2022-12-04 20:31:59 +0900

    定期実行のバグ修正(cronを使うのをやめた)

--- main.go
+++ main.go
@@ -1,24 +1,20 @@
 package main
 
 import (
-	"github.com/robfig/cron/v3"
 	"time"
 	"log"
 	"twitter-bot/internal"
 )
 
 func main() {
-	time.Sleep(time.Second * 10)
 	//dbに接続
 	db, err:= internal.ConnectDb(); 
 	for err != nil {
-		log.Printf("dbの接続に失敗しました: %s", err)
 		db, err = internal.ConnectDb()
 	}
-	c := cron.New()
-    c.AddFunc("@every 1m", func(){internal.Job(db)})
-	c.Start()
+	log.Printf("dbに接続しました")
 	for {
-		time.Sleep(10000000000000)
+		internal.Job(db)
+		time.Sleep(time.Minute)
 	}
 }
